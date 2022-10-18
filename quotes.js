b6ef39764b41bec6cf389415b51046eebcdc893f
AdamMc3011 <113995076+AdamMc3011@users.noreply.github.com>
2022-10-18 15:28:02 +0100

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,47 @@
+[
+{
+    "text": "Concentration produces wealth. Diversification protects wealth ",
+    "author": "James Clear"
+},
+
+{
+    "text": "We are like butterflies who flutter for a day and think it is forever. ",
+    "author": "Carl Sagan"
+},
+
+{
+    "text": "B.H. Liddell Hart’s old line that sometimes the longest way around is the shortest way home. ",
+    "author": "B.H. Liddell"
+},
+
+{
+    "text": "To a disciple who was forever complaining about others, the Master said, ‘If it is peace you want, seek to change yourself, not other people. It is easier to protect your feet with slippers than to carpet the whole of the earth. ",
+    "author": "Anthony de Mello"
+},
+
+{
+    "text": "Whatever you can do, or dream you can, begin it. Boldness has genius, power, and magic in it. Begin it now! ",
+    "author": "Goethe"
+},
+{
+    "text": "Do fewer things. Do them better. Know why you're doing them. ",
+    "author": "Call Newport"
+},
+{
+    "text": "The secret of being miserable is to have the leisure to bother about whether you are happy or not. ",
+    "author": "George Bernard Shaw"
+},
+{
+    "text": "The way of fortune is like the milky way in the sky; which is a number of small stars, not seen asunder, but giving light together: so it is a number of little and scarce discerned virtues, or rather faculties and customs, that make men fortunate. ",
+    "author": "Francis Bacon"
+},
+{
+    "text": "I will not die an unlived life. I will not live in fear of falling or catching fire. I choose to inhabit my days, to allow my living to open me, to make me less afraid, more accessible; to loosen my heart until it becomes a wing, a torch, a promise. I choose to risk my significance, to live so that which came to me as seed goes to the next as blossom, and that which came to me as blossom, goes on as fruit. ",
+    "author": "Dawna Markova"
+},
+{
+    "text": "We have two lives, and the second begins when we realize we only have one. ",
+    "author": "Confucius"
+}
+    
+]
